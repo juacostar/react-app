66f1362e70b5479eb27f96546b70b5d860065d4d
Juan Camilo Acosta <juacostar@unal.edu.co>
2022-05-30 21:37:03 -0500

    creating first component

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap'
+import Menu from './components/MenuComponent';
 import './App.css';
 
 // jsx -> extensiones e javascript para react
@@ -8,12 +9,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
-        </Navbar>  
+        </Navbar>
+        <Menu /> 
     </div>
   );
 }
